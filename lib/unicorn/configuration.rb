fb93a78af6a561899414228104026b60086f9bb5
robertoz-01 <roberto.zanon.01@gmail.com>
2014-08-14 14:55:46 +0200

    added option to dump object space to file before kill for memory limit

--- lib/unicorn/configuration.rb
+++ lib/unicorn/configuration.rb
@@ -1,11 +1,13 @@
 module Unicorn::WorkerKiller
   class Configuration
-    attr_accessor :max_quit, :max_term, :sleep_interval
+    attr_accessor :max_quit, :max_term, :sleep_interval, :object_space_dump_file, :object_space_dump_lock
 
     def initialize
       self.max_quit = 10
       self.max_term = 15
       self.sleep_interval = 1
+      self.object_space_dump_file = '/tmp/unicorn_worker_$PID.json'
+      self.object_space_dump_lock = '/tmp/unicorn_worker_dump.lock'
     end
   end
 end
