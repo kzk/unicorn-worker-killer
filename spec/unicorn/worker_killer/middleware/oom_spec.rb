61661b0e052016e0616653cc3ad74d1afd6af427
Vilius Luneckas <vilius.luneckas@gmail.com>
2018-01-09 07:54:13 +0200

    Add few tests for Unicorn::WorkerKiller::MaxRequests

--- spec/unicorn/worker_killer/middleware/oom_spec.rb
+++ spec/unicorn/worker_killer/middleware/oom_spec.rb
@@ -17,18 +17,41 @@ describe Unicorn::WorkerKiller::Oom do
   describe '#process_client' do
     subject { http_server.process_client(double) }
 
-    it 'increases @_worker_check_count' do
+    it 'increases check counter' do
       expect {
         subject
       }.to change { http_server.instance_variable_get(:@_worker_check_count) }.by(1)
     end
 
-    it 'sets @_worker_process_start' do
+    it 'sets process start timestamp' do
       Timecop.freeze
 
       expect {
         subject
       }.to change { http_server.instance_variable_get(:@_worker_process_start) }.to(Time.now)
+    end
+
+    context 'when check cycle reached' do
+      before { http_server.instance_variable_set(:@_worker_check_count, 2) }
+
+      it 'resets check counter' do
+        allow_any_instance_of(GetProcessMem).to receive_messages(bytes: 0)
+
+        expect {
+          subject
+        }.to change { http_server.instance_variable_get(:@_worker_check_count) }.from(2).to(0)
+      end
+
+      context 'when current memory usage is greater than allowed' do
+        it 'performs suicide' do
+          http_server.instance_variable_set(:@_worker_memory_limit, 100)
+          allow_any_instance_of(GetProcessMem).to receive_messages(bytes: 101)
+
+          expect(Unicorn::WorkerKiller).to receive(:kill_self)
+
+          subject
+        end
+      end
     end
   end
 end
